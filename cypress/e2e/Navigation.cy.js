d0e39e39af11702f4c182ca0a99b250cdd582621
George <George.ashraf@hotmail.com>
2023-04-09 00:22:50 +0200

    Navigation

--- cypress/e2e/Navigation.cy.js
+++ cypress/e2e/Navigation.cy.js
@@ -1,7 +1,30 @@
+// go()
+
 describe('mysuite', ()=>{
 
     it('NavigationTest', ()=>{
 
-        
+        cy.visit('https://demo.opencart.com/')
+        cy.title().should('eq','Your Store')  // homepage
+
+        cy.get('#narbar-menu > ul > li:nth-child(7) > a').click()  // navigate to cameras page
+        cy.get('#content>h2').should('have.text','Cameras')  
+
+        cy.go('back')
+        cy.title().should('eq','Your Store')  // homepage
+
+        cy.go('forward')
+        cy.get('#content>h2').should('have.text','Cameras')  // Cameras page
+
+        cy.go(-1) // go back
+        cy.title().should('eq','Your Store')  // homepage 
+
+        cy.go(1) // go forward
+        cy.get('#content>h2').should('have.text','Cameras')  // cameras page
+
+        cy.reload()
+        cy.get('#content>h2').should('have.text','Cameras')  // cameras page
+
+
     })
 })
